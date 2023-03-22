aa592d1c88bb02e2bf85b1146284a7423dbda91e
jayed-phero <jayed.programminghero@gmail.com>
2023-03-22 03:21:30 -0700

    commit of laerners

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -34,6 +34,7 @@ async function run() {
     try {
         const userCollection = client.db('HeroRider').collection('users')
         const courseCollection = client.db('HeroRider').collection('Courses')
+        const paymentsCollection = client.db('HeroRider').collection('PaymentsInfo')
 
 
 
@@ -64,9 +65,9 @@ async function run() {
         // get single user
         app.get('/singleuser/:email', async (req, res) => {
             const email = req.params.email
-            console.log(email) 
+            console.log(email)
             const query = {
-                email: email 
+                email: email
             }
             const result = await userCollection.findOne(query)
             res.send(result)
@@ -79,6 +80,13 @@ async function run() {
             res.send(result)
         })
 
+
+        app.get('/enrolling/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await courseCollection.findOne(query)
+            res.send(result)
+        })
         // Gell All Courses
 
         app.get('/alluser', async (req, res) => {
@@ -91,12 +99,59 @@ async function run() {
             res.send({ count, userData })
         })
 
+        // course by paid 
+        app.get('/paidstudent/:email', async (req, res) => {
+            const email = req.params.email
+            const query = {
+                email: email
+            }
+            const result = await paymentsCollection.find(query).toArray()
+            res.send(result)
+        })
+
         // admin
         app.get('/users/admin/:email', async (req, res) => {
             const email = req.params.email;
             const query = { email }
             const user = await userCollection.findOne(query);
             res.send({ isAdmin: user?.role === 'admin' });
+        })
+
+        // payment 
+
+        app.post("/create-payment-intent", async (req, res) => {
+            const enrollInfo = req.body;
+            const price = enrollInfo.price
+
+            const amount = price * 100
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        })
+
+        // store payment info 
+        app.post('/payments', async (req, res) => {
+            const payment = req.body
+            const newpaymentInfo = {
+                ...payment,
+                paid: true
+            }
+            const result = await paymentsCollection.insertOne(newpaymentInfo)
+            res.send(result)
+        })
+
+
+        // get payments info 
+        app.get('/paymentsinfo', async (req, res) => {
+            const result = await paymentsCollection.find().toArray()
+            res.send(result)
         })
 
 
